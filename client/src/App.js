7de3470a5f0bd5abb230c64134609f438573fba7
Dmitry <dhr@dbbsoftware.com>
2024-01-05 17:54:38 +0200

    update client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { io } from 'socket.io-client';
-import './App.css';
 
 const socket = io('http://localhost:8000');
 
@@ -18,24 +17,27 @@ export default function App() {
     }
 
     function onFooEvent(value) {
+      console.log('value', value)
       setFooEvents(previous => [...previous, value]);
     }
 
     socket.on('connect', onConnect);
     socket.on('disconnect', onDisconnect);
-    socket.on('foo', onFooEvent);
+    socket.on('HospitalizationCreated', onFooEvent);
+    socket.on('PatientCreated', onFooEvent);
 
     return () => {
       socket.off('connect', onConnect);
       socket.off('disconnect', onDisconnect);
-      socket.off('foo', onFooEvent);
+      socket.off('HospitalizationCreated', onFooEvent);
+      socket.off('PatientCreated', onFooEvent);
     };
   }, []);
 
   return (
     <div className="App">
       <div>Connected: { isConnected.toString() }</div>
-      <div>Events: { fooEvents }</div>
+      <div>Events: { fooEvents.map(e => <div>{e.name}</div>) }</div>
     </div>
   );
 }
